06528e4421dd1eff9c19ffebd72b7e455c176b1d
shaiko_sv <foodprofi@gmail.com>
2020-03-28 22:59:38 +0100

    Laravel - Homework 2 Добавил id='app' для Vue

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div class="container">
+        <div class="container" id="app">
             <!-- Content here -->
             @section('navbar')
 
