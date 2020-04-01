c35abe4378082e92598dae08fef79a1be40646f3
shaiko_sv <foodprofi@gmail.com>
2020-04-01 23:57:12 +0200

    Laravel - Homework 4 Refactoring menu -> adding active class using routeIs() with ternary operator due to ?? don't work :( and request()->path() === 'about'?'active':''

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,15 +1,15 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.newsCrud.index') }}">News</a>
+                    <a class="nav-link {{ request()->routeIs('admin.newsCrud.index')?'active':'' }}" href="{{ route('admin.newsCrud.index') }}">News</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.usersCrud.index') }}">Users</a>
+                    <a class="nav-link {{ request()->routeIs('admin.usersCrud.index')?'active':'' }}" href="{{ route('admin.usersCrud.index') }}">Users</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/laravel">Laravel</a>
+                    <a class="nav-link {{ request()->path() === 'laravel'?'active':'' }}" href="/laravel">Laravel</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('vue') }}">VueJS</a>
+                    <a class="nav-link {{ request()->routeIs('vue')?'active':'' }}" href="{{ route('vue') }}">VueJS</a>
                 </li>
             </ul>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,17 +1,17 @@
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('news.index') }}">News</a>
+                <a class="nav-link {{ request()->routeIs('news.index')?'active':'' }}" href="{{ route('news.index') }}">News</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('news.category.index') }}">Categories</a>
+                <a class="nav-link {{ request()->routeIs('news.category.index')?'active':'' }}" href="{{ route('news.category.index') }}">Categories</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+                <a class="nav-link {{ request()->path() === 'about'?'active':'' }}" href="/about">About</a>
             </li>
             @auth()
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
+                <a class="nav-link {{ request()->routeIs('admin.index')?'active':'' }}" href="{{ route('admin.index') }}">Admin</a>
             </li>
             @endauth
         </ul>
