f5f432a04fbd0c0436399534d26f9e8feef181f8
shaiko_sv <foodprofi@gmail.com>
2020-04-11 21:35:08 +0200

    Laravel - Homework 6 Передала блок Категории под ORM Создал новую миграцию таблицы

--- app/Category.php
+++ app/Category.php
@@ -6,61 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'slug'];
 
-    public static function getCategories()
+    public function news()
     {
-        if(true) {
-            return [];
-        }
-        return [];
-    }
-
-    public static function getCategoryNameById($id)
-    {
-        if (false) {
-            return self::getCategories()[$id]['name'];
-        } else {
-            return Null;
-        }
-    }
-
-    public static function getCategorySlugById($id)
-    {
-        $categories = static::getCategories();
-        return $categories[$id]['slug'];
-    }
-
-    public static function getCategoryIdByName($name)
-    {
-        $id = null;
-        foreach (static::getCategories() as $item) {
-            if($item['slug'] == $name) {
-                $id = $item['id'];
-                break;
-            }
-        }
-        return $id;
-    }
-
-    public static function createCategory($record)
-    {
-        // If slug not informed...
-        if($record['slug'] === null){
-            $record['slug'] = \Str::slug($record['name']); // we use helper to generate it
-        };
-        // Check if category already exists
-        foreach (static::getCategories() as $item){
-            if($item['slug'] == $record['slug']) {
-                return [false, 'Category already exist.'];
-            }
-        }
-        // Return complete record
-        return $record;
-    }
-
-    public static function storeCategory($record)
-    {
-        // Add record to Json file and return ID of new record
-        return  $record;
+        return $this->hasMany('App\News');
     }
 }
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,7 +4,7 @@
                 <a class="nav-link {{ request()->routeIs('news.index')?'active':'' }}" href="{{ route('news.index') }}">News</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('news.category.index')?'active':'' }}" href="{{ route('news.category.index') }}">Categories</a>
+                <a class="nav-link {{ request()->routeIs('news.categories.index')?'active':'' }}" href="{{ route('news.categories.index') }}">Categories</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link {{ request()->path() === 'excel'?'active':'' }}" href="{{ route('excel') }}">Excel</a>
