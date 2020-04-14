71bf05e79360dde8b9dac7a758afe55b76d7112e
shaiko_sv <foodprofi@gmail.com>
2020-04-14 21:23:40 +0200

    Laravel - Homework 7 Валидация создания и обновления новостей. Сделал в отдельном классе валидацию создания и валидацию обновления.

---
+++ app/Http/Requests/StoreNews.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Category;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreNews extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $categoryList = (new Category())->getTable();
+        return [
+            'title' => 'required|min:5|max:20',
+            'shortText' => 'required|min:10',
+            'text' => 'required|min:15',
+            'category_id' => "required|exists:{$categoryList},id",
+            'image' => 'mimes:jpeg,bmp,png|max:1000',
+            'isPrivate' => 'bool',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'title' => 'News title',
+            'shortText' =>'Short description',
+            'text' => 'News\'s text',
+            'category_id' => 'News category',
+            'image' => 'Image',
+            'isPrivate' => 'Private'
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+}
