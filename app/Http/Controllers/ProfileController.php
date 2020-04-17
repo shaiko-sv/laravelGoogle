77225c99a92dff89b8eb53e0cde0856e57872336
shaiko_sv <foodprofi@gmail.com>
2020-04-17 23:17:48 +0200

    "Laravel - Homework 8 Добавил проверку на неверный пароль, и на не соответствие нового пароля с подтверждением пароля.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,19 +14,30 @@ class ProfileController extends Controller
 
         $errors = [];
 
-        if ($request->isMethod('post')) {
-            if ((Hash::check($request->post('password'), $user->password)) && ($request->post('newPassword') === $request->post('password_confirmation'))) {
-                $user->fill([
-                    'name' => $request->post('name'),
-                    'email' => $request->post('email'),
-                    'password' => Hash::make($request->post('newPassword'))
-                ]);
-                $user->save();
+        if ($request->isMethod('post')){
+            if (Hash::check($request->post('password'), $user->password)){
 
+                if ((($request->post('newPassword')) != '' && ($request->post('password_confirmation') != '')) &&
+                    $request->post('newPassword') === $request->post('password_confirmation')) {
+                    $user->fill([
+                        'name' => $request->post('name'),
+                        'email' => $request->post('email'),
+                        'password' => Hash::make($request->post('newPassword'))
+                    ]);
+                } elseif (($request->post('newPassword')) == '' && ($request->post('password_confirmation') == '')) {
+                    $user->fill([
+                        'name' => $request->post('name'),
+                        'email' => $request->post('email')
+                    ]);
+                } else {
+                    $errors['newPassword'][] = 'New password don\'t match.';
+                }
+
+                $user->save();
                 $request->session()->flash('success', 'Profile successfully updated.');
 
             } else {
-                $errors['password'][] = 'Wrong password. Please try again.';
+                $errors['password'][] = 'Wrong password.';
             }
             return redirect()->route('profile')->withErrors($errors);
         }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -49,7 +49,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Old password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="old-password">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="old-password">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -63,7 +63,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="newPassword" type="password" class="form-control @error('password') is-invalid @enderror" name="newPassword" required autocomplete="new-password">
+                                    <input id="newPassword" type="password" class="form-control @error('newPassword') is-invalid @enderror" name="newPassword" autocomplete="new-password">
 
                                     @error('newPassword')
                                     <span class="invalid-feedback" role="alert">
@@ -77,7 +77,7 @@
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm new password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                                 </div>
                             </div>
 
