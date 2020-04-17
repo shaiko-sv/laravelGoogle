5c768f2be370ec2fbbc241816dead03c1e31e605
shaiko_sv <foodprofi@gmail.com>
2020-04-17 23:20:08 +0200

    Laravel - Homework 8 Исправил ошибку удаления категории содержащей новости. Добавил вывод кол-ва новостей у каждой категории Поправил шаблоны

--- app/Category.php
+++ app/Category.php
@@ -12,4 +12,36 @@ class Category extends Model
     {
         return $this->hasMany('App\News');
     }
+
+    /**
+     * @example $category = Post::first();
+     * @example $category->commentsCount; // 4
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne|\Illuminate\Database\Query\Builder
+     */
+    public function newsCount()
+    {
+        return $this->hasOne('App\News')
+            ->selectRaw('category_id, count(*) as aggregate')
+            ->groupBy('category_id');
+    }
+
+//        $category = Category::with('newsCount')->get();
+//        $category->first()->newsCount;
+    /**
+     * @example $category = Category::with('newsCount')->get();
+     * @example $category->first()->newsCount;
+     *
+     * @return int
+     */
+    public function getNewsCountAttribute()
+    {
+        if ( ! array_key_exists('newsCount', $this->relations))
+            $this->load('newsCount');
+
+        $related = $this->getRelation('newsCount');
+
+
+        return ($related) ? (int) $related->aggregate : 0;
+    }
 }
